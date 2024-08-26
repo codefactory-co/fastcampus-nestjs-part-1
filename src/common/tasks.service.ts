ac3691319c6973b1d16ea55f006af931b7f7db58
Jiho Choi <withj1123@gmail.com>
2024-08-26 19:36:10 +0900

    Task Scheduling 02 02 잉여 파일 삭제 기능 구현하기

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -1,12 +1,46 @@
 import { Injectable } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
+import { readdir, unlink } from "fs/promises";
+import { join, parse } from 'path';
 
 @Injectable()
-export class TasksService{
-    constructor(){}
+export class TasksService {
+    constructor() { }
 
-    @Cron('* * * * * *')
-    logEverySecond(){
+    logEverySecond() {
         console.log('1초마다 실행!');
+    }
+
+    // @Cron('* * * * * *')
+    async eraseOrphanFiles() {
+        const files = await readdir(join(process.cwd(), 'public', 'temp'));
+
+        const deleteFilesTargets = files.filter((file) => {
+            const filename = parse(file).name;
+
+            const split = filename.split('_');
+
+            if (split.length !== 2) {
+                return true;
+            }
+
+            try {
+                const date = +new Date(parseInt(split[split.length - 1]));
+                const aDayInMilSec = (24 * 60 * 60 * 1000);
+
+                const now = +new Date();
+
+                return (now - date) > aDayInMilSec;
+
+            } catch (e) {
+                return true;
+            }
+        });
+
+        await Promise.all(
+            deleteFilesTargets.map(
+                (x) => unlink(join(process.cwd(), 'public', 'temp', x))
+            )
+        );
     }
 }
