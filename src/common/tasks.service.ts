bd753fb4a6e55cc4ed6cf8d14f6ddd297df745f6
Jiho Choi <withj1123@gmail.com>
2024-08-26 23:54:35 +0900

    Logging 01 02 Custom Logger 제작하기

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -5,17 +5,26 @@ import { readdir, unlink } from "fs/promises";
 import { join, parse } from 'path';
 import { Movie } from "src/movie/entity/movie.entity";
 import { Repository } from "typeorm";
+import { Logger } from "@nestjs/common";
 
 @Injectable()
 export class TasksService {
+    private readonly logger = new Logger(TasksService.name);
+
     constructor(
         @InjectRepository(Movie)
         private readonly movieRepository: Repository<Movie>,
         private readonly schedulerRegistry: SchedulerRegistry,
     ) { }
 
+    @Cron('*/5 * * * * *')
     logEverySecond() {
-        console.log('1초마다 실행!');
+        this.logger.fatal('FATAL 레벨 로그');
+        this.logger.error('ERROR 레벨 로그');
+        this.logger.warn('WARN 레벨 로그');
+        this.logger.log('LOG 레벨 로그');
+        this.logger.debug('DEBUG 레벨 로그');
+        this.logger.verbose('VERBOSE 레벨 로그');
     }
 
     // @Cron('* * * * * *')
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['verbose'],
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
