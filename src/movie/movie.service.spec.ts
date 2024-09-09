95794e3e5b05c32369688182e96a1cbfed5d160b
Jiho Choi <withj1123@gmail.com>
2024-09-09 16:06:28 +0900

    테스트 04 01 Automock 사용해보기

--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovieService } from './movie.service';
+import { TestBed } from '@automock/jest';
+import { DataSource, Repository } from 'typeorm';
+import { MovieDetail } from './entity/movie-detail.entity';
+import { Movie } from './entity/movie.entity';
+import { Director } from 'src/director/entity/director.entity';
+import { Genre } from 'src/genre/entities/genre.entity';
+import { User } from 'src/user/entities/user.entity';
+import { MovieUserLike } from './entity/movie-user-like.entity';
+import { CommonService } from 'src/common/common.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('MovieService', () => {
-  let service: MovieService;
+  let movieService: MovieService;
+  let movieRepository: jest.Mocked<Repository<Movie>>;
+  let movieDetailRepository: jest.Mocked<Repository<MovieDetail>>;
+  let directorRepository: jest.Mocked<Repository<Director>>;
+  let genreRepository: jest.Mocked<Repository<Genre>>;
+  let userRepository: jest.Mocked<Repository<User>>;
+  let movieUserLikeRepository: jest.Mocked<Repository<MovieUserLike>>;
+  let dataSource : jest.Mocked<DataSource>;
+  let commonService: jest.Mocked<CommonService>;
+  let cacheManager: Cache;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MovieService],
-    }).compile();
+    const {unit, unitRef} = TestBed.create(MovieService).compile()
 
-    service = module.get<MovieService>(MovieService);
+    movieService = unit;
+    movieRepository = unitRef.get(getRepositoryToken(Movie) as string);
+    movieDetailRepository = unitRef.get(getRepositoryToken(MovieDetail) as string);
+    directorRepository = unitRef.get(getRepositoryToken(Director) as string);
+    genreRepository = unitRef.get(getRepositoryToken(Genre) as string);
+    userRepository = unitRef.get(getRepositoryToken(User) as string);
+    movieUserLikeRepository = unitRef.get(getRepositoryToken(MovieUserLike) as string);
+    dataSource = unitRef.get(DataSource);
+    commonService = unitRef.get(CommonService);
+    cacheManager = unitRef.get(CACHE_MANAGER);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(movieService).toBeDefined();
   });
 });
