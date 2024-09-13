914bdffff8052c25b775d1a5b70bc6eb09ea7f77
Jiho Choi <withj1123@gmail.com>
2024-09-13 15:51:57 +0900

    cascade update

--- src/movie/entity/movie-user-like.entity.ts
+++ src/movie/entity/movie-user-like.entity.ts
@@ -12,6 +12,9 @@ export class MovieUserLike {
     @ManyToOne(
         ()=> Movie,
         (movie) => movie.likedUsers,
+        {
+            onDelete: 'CASCADE',
+        }
     )
     movie: Movie;
 
@@ -22,6 +25,9 @@ export class MovieUserLike {
     @ManyToOne(
         ()=> User,
         (user) => user.likedMovies,
+        {
+            onDelete: 'CASCADE',
+        }
     )
     user: User;
 
